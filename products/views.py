d443883e549be8d04d883885e875e182a538e8c2
MarisKX <maris.kurpnieks@gmail.com>
2022-03-19 16:57:03 +0000

    Created product view and base template

--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from .models import Product, Category, Manufacturer, TyreSize
 
-# Create your views here.
+
+def all_products(request):
+    """ A view to return all products page """
+
+    products = Product.objects.all().order_by('price', 'name')
+    categories = Category.objects.all()
+    manufacturers = Manufacturer.objects.all().order_by('display_name', )
+    sizes = TyreSize.objects.all().order_by('rim_size', 'width',)
+
+    context = {
+        'products': products,
+        'categories': categories,
+        'manufacturers': manufacturers,
+        'sizes': sizes,
+    }
+
+    return render(request, 'products/products.html', context)
