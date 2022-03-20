ba0530117b28eb6392bbb534fef7610c80d78a95
MarisKX <maris.kurpnieks@gmail.com>
2022-03-20 00:35:14 +0000

    Added search, sort and filter functionality

--- products/views.py
+++ products/views.py
@@ -1,4 +1,7 @@
+from functools import reduce
+import operator
 from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.db.models import Q
 from .models import Product, Category, Manufacturer, TyreSize
 
 
@@ -10,11 +13,80 @@ def all_products(request):
     manufacturers = Manufacturer.objects.all().order_by('display_name', )
     sizes = TyreSize.objects.all().order_by('rim_size', 'width',)
 
+    query_search = None
+    query_filter_season = None
+    query_filter_size = None
+
+    query_filter_brand = None
+
+    sort = None
+    direction = None
+
+    if request.GET:
+        # Handles free search bar functionality
+        if 'q' in request.GET:
+            query_search = request.GET['q']
+            if not query_search:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(
+                name__icontains=query_search) | Q(
+                description__icontains=query_search) | Q(
+                size__full_size_display__icontains=query_search) | Q(
+                size__full_size_code__icontains=query_search) | Q(
+                size__full_size_short__icontains=query_search)
+            products = products.filter(queries)
+
+        # Handles main filtering functionality
+        if 'season' in request.GET:
+            query_filter_season = request.GET['season']
+            query_filter_size = request.GET['size']
+
+            queries = Q(
+                category__name__icontains=query_filter_season) & Q(
+                size__full_size_code__icontains=query_filter_size)
+            products = products.filter(queries)
+
+        if 'season' and 'brand' in request.GET:
+            query_filter_season = request.GET['season']
+            query_filter_size = request.GET['size']
+            query_filter_brand = request.GET.getlist('brand')
+  
+            queries = Q(
+                category__name__icontains=query_filter_season) & Q(
+                size__full_size_code__icontains=query_filter_size)
+            query_brand = reduce(operator.or_, (Q(
+                manufacturer__name__icontains=each_brand
+                ) for each_brand in query_filter_brand))
+            products = products.filter(queries).filter(query_brand)
+
+        # Handles sorting functionality
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+    selected_sort_method = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'categories': categories,
         'manufacturers': manufacturers,
         'sizes': sizes,
+        'search_term': query_search,
+        'filter_term_season': query_filter_season,
+        'filter_term_size': query_filter_size,
+        'filter_term_brand': query_filter_brand,
+        'selected_sort_method': selected_sort_method,
     }
 
     return render(request, 'products/products.html', context)
