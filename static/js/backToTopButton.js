5fdc6254e2e634a08a05824ebedf6bc8c00f7d86
MarisKX <maris.kurpnieks@gmail.com>
2022-03-25 16:07:15 +0000

    Back to top button visible only after user starts to scroll down window

--- static/js/backToTopButton.js
+++ static/js/backToTopButton.js
@@ -1,3 +1,15 @@
+window.onscroll = function() {scrollFunction()};
+let bttButton = document.getElementsByClassName("btt-button")[0];
+bttButton.style.display = "none";
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    bttButton.style.display = "block";
+  } else {
+    bttButton.style.display = "none";
+  }
+}
+
 $('.btt-link').click(function(e) {
     window.scrollTo(0,0)
 })
