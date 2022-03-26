4e379475f18c34524e18e38e1b4c6c90de587f3d
MarisKX <maris.kurpnieks@gmail.com>
2022-03-26 22:40:40 +0000

    Finished user profile functionality, created send-email function to webhook after successful payment

--- wheelshop_v2/settings.py
+++ wheelshop_v2/settings.py
@@ -197,3 +197,5 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+
+DEFAULT_FROM_EMAIL = 'orders@example.com'
