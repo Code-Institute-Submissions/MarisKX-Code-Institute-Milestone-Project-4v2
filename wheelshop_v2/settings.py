837e37dff60cf1ab08a96eb76fb83441a9b10279
MarisKX <maris.kurpnieks@gmail.com>
2022-03-27 01:25:41 +0000

    Removed Django key from settings

--- wheelshop_v2/settings.py
+++ wheelshop_v2/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xx483ubsp+(9q1zwl-10&peut=n2@@2gr-%_g(-@g(il^!n^ks'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
