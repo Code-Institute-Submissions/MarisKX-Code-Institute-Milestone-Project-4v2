2320b171178ccc505f8999708cd78f2c5de5d34c
MarisKX <maris.kurpnieks@gmail.com>
2022-03-27 03:53:20 +0000

    Removed line 'order_total__iexact=order_total,' from wh handler as it causes error by applying UPPER to integer

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -90,7 +90,6 @@ class StripeWH_Handler:
                     street_address1__iexact=shipping_details.address.line1,
                     street_address2__iexact=shipping_details.address.line2,
                     county__iexact=shipping_details.address.state,
-                    order_total__iexact=order_total,
                     original_cart__iexact=cart,
                     stripe_pid__iexact=pid,
                 )
